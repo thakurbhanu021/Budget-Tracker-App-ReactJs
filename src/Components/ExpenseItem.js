075e183ad83080319e281bc7a93941941d84c308
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-05-09 15:29:43 +0530

    delete button completed

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AppContext } from "../Context";
 
-import {TiDelete} from 'react-icons/ti';
+import { TiDelete } from "react-icons/ti";
 
 const ExpenseItem = (props) => {
-    return (
-        <li className="list-group-item d-flex justify-content-between align-items-center">
-            {props.name}
-            <div>
-                <span className="badge bg-primary rounded-pill mr-3">
-                    ${props.cost}
-                </span>
-                <TiDelete size="1.5em"></TiDelete>
-            </div>
-        </li>
-    )
-}
+  const { dispatch } = useContext(AppContext);
+
+  const handleDelete = () => {
+    dispatch({
+      type: "DELETE_EXPENSE",
+      payload: props.id,
+    });
+  };
+  return (
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      {props.name}
+      <div>
+        <span className="badge bg-primary rounded-pill mr-3">
+          ${props.cost}
+        </span>
+        <TiDelete size="1.5em" onClick={handleDelete}></TiDelete>
+      </div>
+    </li>
+  );
+};
 
 export default ExpenseItem;
