c6903e44a5fd3cb3d954132dc3155a53c355a81e
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-05-11 12:48:52 +0530

    added debouncing logic to search bar

--- src/Components/ExpenseList.js
+++ src/Components/ExpenseList.js
@@ -5,16 +5,37 @@ import { AppContext } from "../Context";
 const ExpenseList = () => {
   const { expenses } = useContext(AppContext);
 
-  const [filteredExpenses, setFilteredExpenses] = useState(expenses || []);
+  // search bar with debouncing logic
 
-  useEffect(()=>{
-    setFilteredExpenses(expenses);
-  },[expenses])
+  const [searchText, setSearchText] = useState("");
 
-  const handleChange = (event) => {
-    const searchResult = expenses.filter((filteredExpense)=> filteredExpense.name.toLowerCase().includes(event.target.value));
-    setFilteredExpenses(searchResult);
-  }
+  const [debouncedValue, setDebouncedValue] = useState(searchText);
+
+  useEffect(() => {
+    if (searchText) {
+      const handler = setTimeout(() => {
+        setDebouncedValue(searchText);
+      }, 500);
+      return () => {
+        clearTimeout(handler);
+      };
+    } else return;
+  }, [searchText]);
+
+  const filteredExpenses = expenses.filter((filteredExpense)=> filteredExpense.name.toLowerCase().includes(debouncedValue));
+
+  //Normal search logic
+
+  //   const [filteredExpenses, setFilteredExpenses] = useState(expenses || []);
+
+  //   useEffect(()=>{
+  //     setFilteredExpenses(expenses);
+  //   },[expenses])
+
+  //   const handleChange = (event) => {
+  //     const searchResult = expenses.filter((filteredExpense)=> filteredExpense.name.toLowerCase().includes(event.target.value));
+  //     setFilteredExpenses(searchResult);
+  //   }
 
   return (
     <>
@@ -22,7 +43,7 @@ const ExpenseList = () => {
         type="text"
         className="form-control mb-2"
         placeholder="Type to search..."
-        onChange={handleChange}
+        onChange={(e) => setSearchText(e.target.value)}
       />
       <ul className="list-group">
         {filteredExpenses.map((expense) => (
