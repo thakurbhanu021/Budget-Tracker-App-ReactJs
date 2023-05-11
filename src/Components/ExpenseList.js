d0fdd33ccf81def90996b95da71f23e89b2b1c13
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-05-11 11:58:52 +0530

    Search bar added

--- src/Components/ExpenseList.js
+++ src/Components/ExpenseList.js
@@ -1,17 +1,40 @@
-import React,  {useContext} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import ExpenseItem from "./ExpenseItem";
 import { AppContext } from "../Context";
 
 const ExpenseList = () => {
-    const {expenses} = useContext(AppContext)
+  const { expenses } = useContext(AppContext);
 
-    return (
-        <ul className="list-group">
-            {expenses.map((expense)=>(
-                <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost}/>
-            ))}
-        </ul>
-    )
-}
+  const [filteredExpenses, setFilteredExpenses] = useState(expenses || []);
+
+  useEffect(()=>{
+    setFilteredExpenses(expenses);
+  },[expenses])
+
+  const handleChange = (event) => {
+    const searchResult = expenses.filter((filteredExpense)=> filteredExpense.name.toLowerCase().includes(event.target.value));
+    setFilteredExpenses(searchResult);
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        className="form-control mb-2"
+        placeholder="Type to search..."
+        onChange={handleChange}
+      />
+      <ul className="list-group">
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            id={expense.id}
+            name={expense.name}
+            cost={expense.cost}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default ExpenseList;
